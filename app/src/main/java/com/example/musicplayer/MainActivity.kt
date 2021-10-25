1229830a54ccb26b4f9b729e3035e71ba49227e7
jadx2 <delmarjaim@gmail.com>
2021-10-24 22:58:51 -0500

    Add stylings to music player section and create the functionality to the play button

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.musicplayer
 
+import android.media.Image
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.databinding.DataBindingUtil
+import com.example.musicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val mediaPlayer = MediaPlayer.create(this, R.raw.cowboys_from_hell )
+
+        val play = findViewById<ImageButton>(R.id.play_btn)
+
+        binding.playBtn.setOnClickListener {
+            if (!mediaPlayer.isPlaying) {
+                mediaPlayer.start()
+                play.setImageResource(R.drawable.pause_arrow)
+            } else {
+                mediaPlayer.pause()
+                play.setImageResource(R.drawable.play_arrow)
+            }
+        }
+
+
     }
 }
